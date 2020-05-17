535f1411765cffae3f526d3fe381f98a9ed502a2
Irvin Chan <irvin@yellowme.mx>
2020-05-17 00:49:58 -0500

    Initial Commit

---
+++ ProfileDetails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProfileDetails
+//
+//  Created by Irvin Geovani Chan Canche on 17/05/20.
+//  Copyright © 2020 iccanche. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
